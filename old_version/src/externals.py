77344349b7b5d5bae5bbb3ca641029de53789431
VV01T3K <wojteks.access+github@gmail.com>
2026-03-21 00:47:53 +0000

    chore: Delete old original version

--- old_version/src/externals.py
+++
@@ -1,6 +0,0 @@
-def process_nav_includes(build_mode, data):
-    """
-    Process navigation data. Previously handled external repositories,
-    now just returns data as-is since externals are not used.
-    """
-    return data
